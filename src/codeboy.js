8b009025fadce37b5f30039208818e2acb8c466e
Remeeus <remeeus@hotmail.com>
2016-07-13 17:05:40 +0200

    added styling and implemented images

--- src/codeboy.js
+++ src/codeboy.js
@@ -1,16 +1,37 @@
 var canvas = document.getElementById("level");
 var context = canvas.getContext("2d");
 
+var skye = {x: 0, y: 0, width: 900, height: 400, color: "#9FABCF"};
 var world = {x: 0, y: 300, width: 900, height: 100, color: "#A1D490"};
 var codeboyPosition = {x: 30, y: 240, width: 30, height: 60, color: "black"};
 var direction = "";
+var imageSun = new Image();
+var imageGrass = new Image();
+// var imageSkye = new Image();
 
-renderBlock(codeboyPosition);
+renderBlock(skye);
 renderBlock(world);
+renderBlock(codeboyPosition);
 
 function renderBlock(position) {
   context.fillStyle = position.color;
   context.fillRect(position.x, position.y, position.width, position.height);
+
+  // imageSkye.onload = function() {
+  //   context.drawImage(imageSkye, 0, 0, 1100, 200);
+  // };
+  // imageSkye.src = 'img/skye.png';
+
+  imageGrass.onload = function() {
+    context.drawImage(imageGrass, -70, 270, 1100, 200);
+  };
+  imageGrass.src = 'img/grass.png';
+
+  imageSun.onload = function() {
+    context.drawImage(imageSun, 825, 25, 50, 50);
+  };
+  imageSun.src = 'img/sun.png';
+
 };
 
 function moveBoy() {
@@ -21,14 +42,15 @@ function moveBoy() {
       var counter = 0;
       var i = setInterval(function(){
         context.clearRect(0, 0, canvas.width, canvas.height);
-        codeboyPosition.y -= 20;
-        renderBlock(codeboyPosition);
+        codeboyPosition.y -= 5;
+        renderBlock(skye);
         renderBlock(world);
+        renderBlock(codeboyPosition);
           counter++;
-          if(counter === 3) {
+          if(counter === 12) {
               clearInterval(i);
           }
-      }, 200);
+      }, 12);
     break;
 
     case "down":
@@ -36,14 +58,16 @@ function moveBoy() {
 
     case "left":
       codeboyPosition.x -= 30;
-      renderBlock(codeboyPosition);
+      renderBlock(skye);
       renderBlock(world);
+      renderBlock(codeboyPosition);
     break;
 
     case "right":
       codeboyPosition.x += 30;
-      renderBlock(codeboyPosition);
+      renderBlock(skye);
       renderBlock(world);
+      renderBlock(codeboyPosition);
     break;
   }
 };
